0748055fa8933ab7abd2c33ffcc82fd47a9bd96a
Oksana <derbasova.2005@mail.ru>
2021-06-29 00:56:50 +0600

    update user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,10 +18,8 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'last_name',
         'email',
         'password',
-        'dob'
     ];
 
     /**
