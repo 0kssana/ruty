f66f9b12b7541a8eb1bab5ee15331471b664b3e7
Oksana <derbasova.2005@mail.ru>
2021-06-29 01:02:15 +0600

    update post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    function comments(){
+        return $this->hasMany('App\Models\Comment')->orderBy('id','desc');
+    }
+
+    function category(){
+        return $this->belongsTo('App\Models\Category','cat_id');
+    }
 }
