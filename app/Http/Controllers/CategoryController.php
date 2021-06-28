c360e3eaa68694abb18915154799c9476b5d8e54
Oksana <derbasova.2005@mail.ru>
2021-06-29 02:56:08 +0600

    added function index

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,7 +4,22 @@
 namespace App\Http\Controllers;
 
 
+use App\Models\Category;
+
 class CategoryController
 {
-
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data=Category::all();
+        return view('backend.category.index',[
+            'data'=>$data,
+            'title'=>'All Categories',
+            'meta_desc'=>'This is meta description for all categories'
+        ]);
+    }
 }
