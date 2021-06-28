8ffe8d1f0e564993beb24ec9b47aeab7995b11d8
Oksana <derbasova.2005@mail.ru>
2021-06-29 02:43:44 +0600

    added routing for methods in indexcontroller

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,10 @@ Route::get('/', function () {
 });*/
 
 Route::get('/',[IndexController::class,'index']);
+Route::get('/detail/{slug}/{id}',[IndexController::class,'detail']);
+Route::get('/all-categories',[IndexController::class,'all_category']);
+Route::get('/category/{slug}/{id}',[IndexController::class,'category']);
+Route::post('/save-comment/{slug}/{id}',[IndexController::class,'save_comment']);
+Route::get('save-post-form',[IndexController::class,'save_post_form']);
+Route::post('save-post-form',[IndexController::class,'save_post_data']);
+Route::get('manage-posts',[IndexController::class,'manage_posts']);
