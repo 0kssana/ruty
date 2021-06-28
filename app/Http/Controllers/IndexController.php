fe28c3c5294c6f1918b985ff6a49da1dcdd7d071
Oksana <derbasova.2005@mail.ru>
2021-06-29 01:25:19 +0600

    added index method

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,7 +4,18 @@
 namespace App\Http\Controllers;
 
 
-class IndexController
-{
+use App\Models\Post;
+use Illuminate\Http\Request;
 
+class IndexController extends Controller
+{
+    function index(Request $request) {
+        if($request->has('q')) {
+            $q=$request->q;
+            $posts=Post::where('title','like','%'.$q.'%')->orderBy('id','desc')->paginate(2);
+        }else{
+            $posts=Post::orderBy('id','desc')->paginate(2);
+        }
+        return view('home',['posts'=>$posts]);
+    }
 }
