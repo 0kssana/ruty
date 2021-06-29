22fe49e4bc648ae8c2ae0f8442e1234d4a70b443
Oksana <derbasova.2005@mail.ru>
2021-06-29 19:09:39 +0600

    update methods 1.8

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -1,18 +1,17 @@
 <?php
 
-
 namespace App\Http\Controllers;
 
-
+use Illuminate\Http\Request;
+use App\Models\Post;
 use App\Models\Category;
 use App\Models\Comment;
-use App\Models\Post;
-use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
-    function index(Request $request) {
-        if($request->has('q')) {
+    function index(Request $request){
+        // $posts=Post::orderBy('id','desc')->simplePaginate(1);
+        if($request->has('q')){
             $q=$request->q;
             $posts=Post::where('title','like','%'.$q.'%')->orderBy('id','desc')->paginate(2);
         }else{
@@ -27,17 +26,20 @@ class IndexController extends Controller
         $detail=Post::find($postId);
         return view('detail',['detail'=>$detail]);
     }
+
     // All Categories
     function all_category(){
         $categories=Category::orderBy('id','desc')->paginate(5);
         return view('categories',['categories'=>$categories]);
     }
+
     // All posts according to the category
     function category(Request $request,$cat_slug,$cat_id){
         $category=Category::find($cat_id);
         $posts=Post::where('cat_id',$cat_id)->orderBy('id','desc')->paginate(2);
         return view('category',['posts'=>$posts,'category'=>$category]);
     }
+
     // Save Comment
     function save_comment(Request $request,$slug,$id){
         $request->validate([
